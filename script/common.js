05c95a3fb4245d6f733d2b1ebf7c2e36522e9d55
cherish <cherish@gmail.com>
2017-03-10 22:49:55 +0800

    commit

--- script/common.js
+++ script/common.js
@@ -1,6 +1,20 @@
-/*$(document).ready(function(){
-	$(document).on("click", "button[data-toggle='modal']", function(){
+$(document).ready(function(){
+	/*$(document).on("click", "button[data-toggle='modal']", function(){
 		alert("1");
+	});*/
+	
+	$(".datepicker").datepicker({
+		format: 'yyyy/mm/dd'
 	});
+	
+	if($("#search").length > 0){
+		$(document).on("click", ".action-add", function(){
+			var $item = $("#template-searchItem").html();
+			$(this).closest(".searchItem").after($item);
+		});
+		
+		$(document).on("click", ".action-remove", function(){
+			$(this).closest(".searchItem").remove();
+		});
+	}
 });
-*/
