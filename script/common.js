f1a3162000702549fcc5ef6a17751846fa350547
cherish <cherish@gmail.com>
2017-03-12 23:59:53 +0800

    commit

--- script/common.js
+++ script/common.js
@@ -1,13 +1,10 @@
-$(document).ready(function(){
-	/*$(document).on("click", "button[data-toggle='modal']", function(){
-		alert("1");
-	});*/
+/*Dom Ready*/
+$(document).ready(function() {
+	$(".chzn-select").chosen();
 	
-	$(".datepicker").datepicker({
-		format: 'yyyy/mm/dd'
-	});
-	
-	if($("#search").length > 0){
+	/* search */
+	if($("#searchPage").length > 0) {
+		// add search keywords
 		$(document).on("click", ".action-add", function(){
 			var $item = $("#template-searchItem").html();
 			$(this).closest(".searchItem").after($item);
@@ -15,6 +12,33 @@ $(document).ready(function(){
 		
 		$(document).on("click", ".action-remove", function(){
 			$(this).closest(".searchItem").remove();
+		});
+		
+		
+		// datepicker
+		$(".datepicker").datepicker({
+			format: 'yyyy/mm/dd'
+		})
+	}
+	
+	if($("#dataInputPage").length > 0) {
+		$(document).on("click", "#basicData-form button#check-form", function(){
+			var dataObj = $("#basicData-form").serializeArray();
+			var i = 0;
+			$(".modal-checkInput ul li span").each(function(){
+				$(this).text(dataObj[i].value);
+				i++;				
+			});
+			
+			$("button[data-target='.modal-checkInput']").click();
+			
+			return false;
+		});
+		
+		$(document).on("click", ".modal-checkInput button.sureToSubmit", function(){
+			$("#basicData-form").submit(function(e){
+				alert("已提交！");
+			});
 		});
 	}
 });
